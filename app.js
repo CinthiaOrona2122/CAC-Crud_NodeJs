2cfdcec7aad73763d41f898f4d7b3962af4fd960
Cinthia M. Orona <devrider2021@gmail.com>
2022-06-15 14:31:09 -0300

    Practicas Clase 05, rutas, vistas, validaciones de formularios, distribucion modular.

--- app.js
+++ app.js
@@ -9,13 +9,17 @@ app.use(expressLayouts); //middleware
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(require("./router"))
+app.use(express.urlencoded({ extended: false})); //toma los datos que vienen del formulario
+//true = para forms largos
+
+
+app.use(require("./routes/index"));
+app.use(require("./routes/productos"));
+app.use(require("./routes/contacto"));
 
 app.use((req, res, next) => {
   res.status(404).send("Not found");
 });
-
-
 
 const port = process.env.PORT || 3000;
 
