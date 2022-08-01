b9c080550b0546c437e3209e1ad3318bfd3906f6
Cinthia M. Orona <devrider2021@gmail.com>
2022-08-01 06:03:43 -0300

    Clase 29: CRU, faltó hacer el Delete.

--- controllers.js
+++ controllers.js
@@ -2,38 +2,55 @@ const connection = require('./db');
 
 
 module.exports.index = (req, res) => {
-    connection.query('SELECT id, nombre, descripcion FROM productos', (error, results) => {
-        if(error) throw error;
+    connection.query('SELECT * FROM productos', (err, results) => {
+        if(err) throw err;
         res.render('productos/index', { productos: results });
     });
 }
 
-
+//muestra formulario
 module.exports.create = (req, res) => {
   res.render('productos/create');
 }
 
+//guarda nuevo producto
 module.exports.store = (req, res) => {
   connection.query('INSERT INTO productos SET ?', 
-  { nombre: req.body.nombre, categoria_id: req.body.categoria},  
-    (error, results) => {
+  { nombre: req.body.nombre, categoria_id: req.body.categoria, descripcion: req.body.descripcion},  
+    (err, results) => {
 
-    if(error) throw error;
+    if(err) throw err;
 
     res.redirect('/productos');
     console.log(results);
 });
-}
+} 
 
+//muestra el producto segun el id
 module.exports.show = (req, res) => {
-  connection.query('SELECT * FROM productos WHERE id = ?', [req.params.id],(error, results) => {
-      if(error) throw error;
+  connection.query('SELECT * FROM productos WHERE id = ?', [req.params.id],(err, results) => {
+      if(err) throw err;
       
       res.render('productos/show', { producto: results[0] });
-    console.log(results)
+    //console.log(results)
   });
 }
 
 module.exports.edit = (req, res) => {
-  res.render('productos/edit');
+  connection.query('SELECT * FROM productos WHERE id = ?', [req.params.id], (err, results) => {
+    if(err) throw err;
+
+    res.render('productos/edit', { producto: results[0] });
+    //console.log(results);
+  })
+}
+
+module.exports.update = (req, res) => {
+  connection.query('UPDATE productos SET ? WHERE id = ?', [{ nombre: req.body.nombre, descripcion: req.body.descripcion ,categoria_id: req.body.categoria}, req.body.id], (err) => {
+    if(err) throw err;
+
+    res.redirect('/productos');
+    console.log('Producto actualizado');
+  }
+  )
 }
--- routes/productos.js
+++ routes/productos.js
@@ -9,8 +9,10 @@ router.get('/productos/create', controller.create);
 
 router.post('/productos/store', controller.store);
 
-router.get('/productos/:id', controller.show);
+router.get('/productos/:id', controller.show); 
 
 router.get('/productos/:id/edit', controller.edit);
+
+router.put('/productos/update', controller.update);
 
 module.exports = router;
